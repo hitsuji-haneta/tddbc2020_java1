a56771f4796a33c6a789f7f7a9125ffe5508f44f
hitsuji-haneta <hitsuji-haneta@hotmail.com>
2020-08-01 14:15:14 +0900

    nest test

--- src/main/java/tddbc/ClosedRange.java
+++ src/main/java/tddbc/ClosedRange.java
@@ -5,8 +5,10 @@ public class ClosedRange {
   private int _topPoint;
 
   public ClosedRange(int bottomPoint, int topPoint) {
+    if (bottomPoint > topPoint) {
+      throw new IllegalArgumentException("下端点が上端点より大きいです。");
+    }
     _bottomPoint = bottomPoint;
     _topPoint = topPoint;
-
   }
 }
--- src/test/java/tddbc/ClosedRangeTest.java
+++ src/test/java/tddbc/ClosedRangeTest.java
@@ -3,13 +3,16 @@ package tddbc;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Nested;
 
 public class ClosedRangeTest {
 
-    @Test
-    public void 下端点が上端点より大きい場合はエラー() throws Exception {
-//    	ClosedRange closeRange = new ClosedRange(3, 2);
-    	assertThrows(IllegalArgumentException.class, () -> new ClosedRange(3, 2));
+    @Nested
+    class 上端点より下端点が大きい閉区間を作ることはできない {
+        @Test
+        public void 下端点が上端点より大きい場合はエラー() throws Exception {
+            assertThrows(IllegalArgumentException.class, () -> new ClosedRange(3, 2));
+        }
     }
 
 }
