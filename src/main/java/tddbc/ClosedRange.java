5f8e0c84b8b63517f2e1d2bfd519ffda83125e55
hitsuji-haneta <hitsuji-haneta@hotmail.com>
2020-08-01 13:40:07 +0900

    wip: create ClosedRange class

---
+++ src/main/java/tddbc/ClosedRange.java
@@ -0,0 +1,11 @@
+package tddbc;
+
+public class ClosedRange {
+  private int _bottomPoint;
+  private int _topPoint;
+
+  public ClosedRange(int bottomPoint, int topPoint) {
+    _bottomPoint = bottomPoint;
+    _topPoint = topPoint;
+  }
+}
---
+++ src/test/java/tddbc/ClosedRangeTest.java
@@ -0,0 +1,14 @@
+package tddbc;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ClosedRangeTest {
+
+    @Test
+    public void 失敗() throws Exception {
+        fail();
+    }
+
+}
